e7ee398dd918ff0314b63bd64fee422f5410313b
mahankabir <mahan.kabir@gmail.com>
2025-07-17 13:04:42 +0330

    feat(user): add user model.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // فقط برای توسعه! برای production حتماً دامنه دقیق بذار
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 9060, '0.0.0.0');
 }
 bootstrap();
